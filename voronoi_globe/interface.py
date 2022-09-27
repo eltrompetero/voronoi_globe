19b04551361689dfb97d005e5a4f892bfd493446
Eddie Lee <edlee@csh.ac.at>
2022-09-27 13:12:13 +0200

    added option to exclude African boundaries or center

--- voronoi_globe/interface.py
+++ voronoi_globe/interface.py
@@ -2,11 +2,13 @@
 # Interface functions.
 # Author: Eddie Lee, edlee@csh.ac.at
 # ====================================================================================== #
+from fiona.errors import DriverError
+
 from .utils import *
 
 
 
-def load_voronoi(dx, gridix=0, prefix='.'):
+def load_voronoi(dx, gridix=0, prefix='.', exclude_boundary=False, exclude_center=False):
     """Load GeoPandas DataFrame and apply proper index before returning.
 
     Parameters
@@ -15,11 +17,15 @@ def load_voronoi(dx, gridix=0, prefix='.'):
     gridix : int, 0
     prefix : str, ''
         Directory prefix.
+    exclude_boundary : bool, False
+    exclude_center : bool, False
 
     Returns
     -------
     gpd.GeoDataFrame
     """
+
+    assert not (exclude_center and exclude_boundary)
 
     gdf = gpd.read_file(f'{prefix}/voronoi_grids/{dx}/borders{str(gridix).zfill(2)}.shp')
     with open(f'{prefix}/voronoi_grids/{dx}/borders_ix{str(gridix).zfill(2)}.p', 'rb') as f:
@@ -28,7 +34,20 @@ def load_voronoi(dx, gridix=0, prefix='.'):
     
     # turn neighbor strings into lists
     gdf['neighbors'] = gdf['neighbors'].apply(lambda x: [int(i) for i in x.split(', ')])
-
+    
+    try:
+        if exclude_boundary:
+            af = gpd.read_file('continent-poly/Africa_main.shp')
+            contained_ix = [af['geometry'].iloc[0].contains(p.buffer(1))
+                            for p in gdf['geometry'].values]
+            gdf = gdf.loc[contained_ix]
+        elif exclude_center:
+            af = gpd.read_file('continent-poly/Africa_main.shp')
+            contained_ix = [not af['geometry'].iloc[0].contains(p.buffer(1))
+                            for p in gdf['geometry'].values]
+            gdf = gdf.loc[contained_ix]
+    except DriverError:
+        warn("Africa shapefile cannot be found. Boundary filtering not done.")
     return gdf
 
 def load_centers(dx, gridix=0, prefix='.'):
